4390eb2d36faaeb99120aae68d580d834252b4ee
Stanislav Demchenko <stanislav.demchenko@omnigon.com>
2017-04-19 16:22:50 +0300

    Minor UI fixes for ttmaster.com.ua.

--- rating_calculator_ui_wp.js
+++ rating_calculator_ui_wp.js
@@ -1,8 +1,12 @@
 
-/* Remove garbage inserted by WordPress into the calculator integrated into http://ttmaster.com.ua/ */
+/* UI cleanup for the calculator integrated into http://ttmaster.com.ua/ */
 stateCheckForTtMaster = setInterval(function () {
     if (document.readyState === 'complete') {
         clearInterval(stateCheckForTtMaster);
+        /* Remove garbage inserted by WordPress */
         $(".match br").remove();
+        /* Replace the first (misaligned) match with a good-looking one. */
+        addMatch();
+        $(".match:first").parent().remove();
     }
 }, 100);
